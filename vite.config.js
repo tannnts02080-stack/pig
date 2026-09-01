33a90b7f54f642d36477a14b922674cb4e411bf2
tannnts02080-stack <tannnts02080@gmail.com>
2026-09-01 16:33:34 +0700

    fix: proxy target to localhost 127.0.0.1:8080 and enable right click

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: true, // Listen on all local IPs
     proxy: {
       '/api': {
-        target: 'http://172.21.154.88:8080', // IP laptop để mobile có thể kết nối
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true,
       }
     }
